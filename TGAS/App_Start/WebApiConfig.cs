813df35e242180681f607993fe63fb46378ba247
zero1036 <799162239@qq.com>
2015-07-14 18:33:29 +0800

    lzc:创建集成项目

---
+++ TGAS/App_Start/WebApiConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace TGAS
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            //return json format camelCase to Client
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            ////CORS
+            //var cors = new EnableCorsAttribute("*", "*", "*");
+            //cors.SupportsCredentials = true;
+            //config.EnableCors(cors);
+
+            //// Web API routes
+            //config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{param}",
+                defaults: new { param = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ TGAS/Controllers/Base/ApiControllerBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Security;
+using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+
+namespace TGAS.Controllers
+{
+    /// <summary>
+    /// Controller的基类，用于实现适合业务场景的基础功能
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    [BasicAuthentication]
+    public abstract class ApiControllerBase : ApiController
+    {
+        /// <summary>
+        /// 执行方法并捕获错误
+        /// </summary>
+        /// <param name="pInterface"></param>
+        /// <param name="action"></param>
+        protected HttpResponseMessage ExecuteTryCatch<VModel>(Func<VModel> action)
+        {
+            try
+            {
+                var vm = action.Invoke();
+                var result = new { ok = true, message = "", data = vm };
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception ex)
+            {
+                var result = new { ok = false, message = ex.Message };
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+        }
+    }
+
+    /// <summary>
+    /// UserAuthModel
+    /// </summary>
+    public class UserAuthModel
+    {
+        public string Controller
+        {
+            get;
+            set;
+        }
+
+        public string Actions
+        {
+            get;
+            set;
+        }
+    }
+}
