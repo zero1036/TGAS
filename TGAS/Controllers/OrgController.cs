ddfc330dd360b2aba4651ea2f885adf77bda6ed1
zero1036 <799162239@qq.com>
2015-08-03 09:40:55 +0800

    lzc:添加OrgCtrl

---
+++ TGAS/Controllers/OrgController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TGAS.Controllers
+{
+    public class OrgController : ApiController
+    {
+        public bool Login()
+        {
+            return true;
+        }
+    }
+}
