24d7d15a85b0718944b8b96b7c1671a4a5186298
zero1036 <t799162239@hotmail.com>
2015-08-09 19:27:33 +0800

    lzc:停用build版本

--- TGAS/app/scripts/routes/partnerMain.js
+++ TGAS/app/scripts/routes/partnerMain.js
@@ -1,36 +1,36 @@
-﻿//require.config({
-//    paths: {
-//        // Angular
-//        angular: '../vendor/angular/angular',
-//        cookies: '../vendor/angular/angular-cookies',
-//        route: '../vendor/angular/angular-route',
-//        touch: '../vendor/angular/angular-touch',
-//        // domReady
-//        domReady: '../vendor/domReady',
-//        // iscroll
-//        iscroll: '../vendor/iscroll/iscroll'
-//    },
-//    shim: {
-//        /*
-//         *对引入的类库，表明依赖关系，并暴露名称
-//         *需要把此处的名字，在app.js文件中define写上才能使用
-//         */
-//        angular: {
-//            //deps: ['jquery'],
-//            exports: 'angular'
-//        },
-//        cookies: {
-//            deps: ['angular']
-//        },
-//        route: {
-//            deps: ['angular']
-//        },
-//        touch: {
-//            deps: ['angular']
-//        }
-//    },
-//    waitSeconds: 0
-//});
+﻿require.config({
+    paths: {
+        // Angular
+        angular: '../vendor/angular/angular',
+        cookies: '../vendor/angular/angular-cookies',
+        route: '../vendor/angular/angular-route',
+        touch: '../vendor/angular/angular-touch',
+        // domReady
+        domReady: '../vendor/domReady',
+        // iscroll
+        iscroll: '../vendor/iscroll/iscroll'
+    },
+    shim: {
+        /*
+         *对引入的类库，表明依赖关系，并暴露名称
+         *需要把此处的名字，在app.js文件中define写上才能使用
+         */
+        angular: {
+            //deps: ['jquery'],
+            exports: 'angular'
+        },
+        cookies: {
+            deps: ['angular']
+        },
+        route: {
+            deps: ['angular']
+        },
+        touch: {
+            deps: ['angular']
+        }
+    },
+    waitSeconds: 0
+});
 require([
 	'angular',
 	'partnerApp',
